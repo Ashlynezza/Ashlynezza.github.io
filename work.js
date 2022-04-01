a215a8adbb4f22291c078f6421c7daf7e3902d8a
Ashlynezza <ashlynx0512@gmail.com>
2022-04-01 18:00:30 -0400

    1

--- work.js
+++ work.js
@@ -1,2 +1,14 @@
+function dis
 alert(Date());
+
+
 onmouseover="https://p3-tt.byteimg.com/origin/pgc-image/4c84296e66a84b6da958cd9133dcecd9";
+
+fuction changeText(a){
+document.getElementById('one').innerHTML="Bye Bye";
+}
+
+fuction showKeycode(b){
+	alert(a.keycode);
+}
+document.onKeydown=showKeycode
