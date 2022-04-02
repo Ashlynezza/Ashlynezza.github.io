10baf19ada96b89b37e3154842577d2b24274c9b
Ashlynezza <ashlynx0512@gmail.com>
2022-04-01 21:04:06 -0400

    1

--- work.js
+++ work.js
@@ -1,14 +1,33 @@
-function dis
 alert(Date());
 
-
-onmouseover="https://p3-tt.byteimg.com/origin/pgc-image/4c84296e66a84b6da958cd9133dcecd9";
-
-fuction changeText(a){
+function over(){
+	document.getElementById('two').src='https://p3-tt.byteimg.com/origin/pgc-image/4c84296e66a84b6da958cd9133dcecd9';
+}
+function changeText(){
 document.getElementById('one').innerHTML="Bye Bye";
 }
 
-fuction showKeycode(b){
-	alert(a.keycode);
+function Keycode(){
+	alert()
 }
-document.onKeydown=showKeycode
+
+function change(){
+	var select=document.getElementById('three');
+	select.style.backgroundColor='pink';
+	select.style.fontFamily='Arial';
+	select.style.textAlign='center';
+}
+function temp(f){
+	var f=20;
+	alert((f-32)/1.8);
+}
+function cal(){
+	var a = parseFloat(prompt('the frist number'));
+	var b = parseFloat(prompt('the second number'));
+	console.log(a+b);
+}
+function sum(){
+	var c = prompt('What is your First Name')
+	var d= prompt('What is your second Name')
+	alert("Nice to meet you"+c+d)
+}
