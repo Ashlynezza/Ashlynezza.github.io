a60c6f93811a8aef926f15aaa195c85dae6a81d4
Ashlynezza <ashlynx0512@gmail.com>
2022-04-04 09:52:04 -0400

    h

--- work.js
+++ work.js
@@ -18,7 +18,7 @@ function change(){
 	select.style.textAlign='center';
 }
 function temp(f){
-    return (f-32)*5/9;
+    return (f-32)*(5/9);
     document.getElementById('degree').innerHTML=temp;
 }
 
