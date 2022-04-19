a77ba7330f8478f6888e01267ab965e785ee8bfa
Ashlynezza <ashlynx0512@gmail.com>
2022-04-19 17:07:16 -0400

    7

--- Alien3.js
+++ Alien3.js
@@ -1,1 +1,10 @@
-大ndfmbhjawmfjdawshufawhuir
+function scrollWindow(){
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function nameee(){
+        var three = prompt('First Name');
+        var four = prompt('Last Name');
+        var str =('Congratulations to welcome in' + ' ' + three +' '+ four)
+        alert(str);
+}
